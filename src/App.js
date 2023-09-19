56f5cf7392878a37a7c642a48fb6a4c6674b0ed7
Thaiza Capelão <thaizacapelao@gmail.com>
2023-09-19 16:50:30 -0400

    finalizando alteracoes visuais

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import * as XLSX from "xlsx";
+import { CiExport } from "react-icons/ci";
 
 function App() {
-
-
   // onChange estados:
 
   const [excelFile, setExcelFile] = useState(null);
@@ -14,50 +13,47 @@ function App() {
   const [excelData, setExcelData] = useState(null);
 
   // onChange evento:
-  const handlefile=(e)=> {
-    let fileTypes=[
+  const handlefile = (e) => {
+    let fileTypes = [
       "application/vnd.ms-excel",
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
       "text/csv",
       "text/plain",
     ];
-    let selectedFile=e.target.files[0];
+    let selectedFile = e.target.files[0];
     if (selectedFile) {
       if (selectedFile && fileTypes.includes(selectedFile.type)) {
         setTypeError(null);
         let reader = new FileReader();
         reader.readAsArrayBuffer(selectedFile);
         reader.onload = (e) => {
-          document.querySelector('.text').textContent = selectedFile.name;
+          document.querySelector(".text").textContent = selectedFile.name;
           setExcelFile(e.target.result);
         };
-      } 
-      else {
+      } else {
         setTypeError("Por favor selecione apenas arquivos de excel ou txt");
         setExcelFile(null);
       }
-    } 
-    else {
+    } else {
       console.log("Por favor Selecione seu arquivo");
     }
-  }
+  };
 
   // submit evento
-  const handlefileSubmit=(e)=>{
+  const handlefileSubmit = (e) => {
     e.preventDefault();
-    if(excelFile!==null){
-      const workbook = XLSX.read(excelFile,{type: 'buffer'});
+    if (excelFile !== null) {
+      const workbook = XLSX.read(excelFile, { type: "buffer" });
       const worksheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[worksheetName];
       const data = XLSX.utils.sheet_to_json(worksheet);
-      setExcelData(data.slice(0,10));
+      setExcelData(data.slice(0, 10));
       setCenterViewerContent(true);
     }
-  }
+  };
 
   return (
     <div className="wrapper">
-
       <h3 className="titulo">Carregar e visualizar planilhas de Excel</h3>
 
       {/* Form */}
@@ -71,44 +67,51 @@ function App() {
         />
         <label for="file">
           <span class="text">Selecione o arquivo</span>
-          <span>Procurar</span>
+          <span>
+          <CiExport className="custom-icon" strokeWidth={2} size={22} /> Browse Files
+          </span>
         </label>
-        <button 
-        type="submit" 
-        className="btn btn-sucess btn-md"
-        onClick={handlefileSubmit}
+        <button
+          type="submit"
+          className="btn btn-sucess btn-md"
+          onClick={handlefileSubmit}
         >
-          CARREGAR</button>
-        {typeError&&(
-          <div className="alert alert-danger custom-error" role="alert">{typeError}</div>
+         CARREGAR
+        </button>
+        {typeError && (
+          <div className="alert alert-danger custom-error" role="alert">
+            {typeError}
+          </div>
         )}
       </form>
 
       {/* View data */}
-      <div className={`viewer ${centerViewerContent ? 'center-content' : ''}`}>
-        {excelData?(
+      <div className={`viewer ${centerViewerContent ? "center-content" : ""}`}>
+        {excelData ? (
           <div className="table-responsive">
             <table className="table">
               <thead>
                 <tr>
-                  {Object.keys(excelData[0]).map((key)=>(
-                  <th key={key}>{key}</th>
+                  {Object.keys(excelData[0]).map((key) => (
+                    <th key={key}>{key}</th>
                   ))}
                 </tr>
               </thead>
               <tbody>
-                {excelData.map((individualExcelData, index)=>(
-                    <tr key={index}>
-                      {Object.keys(individualExcelData).map((key)=>(
-                        <td key={key}>{individualExcelData[key]}</td>
-                      ))}
-                    </tr>
-                  ))}
+                {excelData.map((individualExcelData, index) => (
+                  <tr key={index}>
+                    {Object.keys(individualExcelData).map((key) => (
+                      <td key={key}>{individualExcelData[key]}</td>
+                    ))}
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
         ) : (
-          <div >Nenhum arquivo foi carregado ainda.</div>
+          <div className="texto-inicial">
+            Nenhum arquivo foi carregado ainda.
+          </div>
         )}
       </div>
     </div>
